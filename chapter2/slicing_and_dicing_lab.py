bb6ec16e6f5107bf7e29c911e555420c06a8e32c
khanhlq <khanh96le@gmail.com>
2017-01-19 07:17:55 +0700

    lab combining data

--- chapter2/slicing_and_dicing_lab.py
+++ chapter2/slicing_and_dicing_lab.py
@@ -8,4 +8,8 @@ ratings = pd.read_table('data/ml-1m/ratings.dat', sep='::', header=None, names=r
 
 mnames = ['movie_id', 'title', 'genres']
 movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=mnames)
+
+
+data = pd.merge(pd.merge(ratings, users), movies)
+mean_ratings = data.pivot_table('rating', rows='title', cols='gender', aggfuc='mean')
 print 0
