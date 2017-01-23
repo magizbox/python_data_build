f77848f92a762304af6775000f787129279bcd54
KhanhLQ <khanhlq@git.mv>
2017-01-23 11:17:36 +0700

    refactor directory of chapter2

---
+++ chapter3/ipython_log.py
@@ -0,0 +1,101 @@
+# IPython log file
+
+b = pd.DataFrame({
+        "name": ['john', 'mary', 'anna'],
+        "age": [25, 23, 19]
+    })
+get_ipython().magic(u'pinfo b')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {i : np.random.randn() for i in range(7)}
+data
+an_apple = 15
+an_apple = 30
+b = [1, 2, 3]
+b.append(3)
+b.count(3)
+b.__doc__
+path = '/home/khanhlq/PycharmProjects/python_data_build/chapter3/'
+b = pd.DataFrame({
+        "name": ['john', 'mary', 'anna'],
+        "age": [25, 23, 19]
+    })
+get_ipython().magic(u'pinfo b')
+a = 1
+get_ipython().magic(u'pinfo a')
+def add_numbers(a, b):
+    """
+    Add two numbers
+    
+    Returns: the sum of two numbers
+    """
+    return a + b
+get_ipython().magic(u'pinfo add_numbers')
+get_ipython().magic(u'pinfo2 pd.pivot_table')
+get_ipython().magic(u'run script.py')
+result
+get_ipython().magic(u"run sys_lab.py 12 89 'abc'")
+pront
+b = pd.DataFrame({
+        "name": ['john', 'mary', 'anna'],
+        "age": [25, 23, 19]
+    })
+get_ipython().magic(u'pinfo b')
+get_ipython().magic(u'run script.py')
+a = np.random.randn(100, 100)
+a
+get_ipython().magic(u'timeit np.dot(a, a)')
+get_ipython().magic(u'quickref')
+get_ipython().magic(u'alias d ls -alh')
+get_ipython().magic(u'd ')
+get_ipython().magic(u'alias d dir')
+get_ipython().magic(u'd ')
+dir
+get_ipython().magic(u'run dir')
+get_ipython().magic(u'alias d dir')
+dir
+get_ipython().system(u'dir')
+get_ipython().magic(u'hist')
+get_ipython().magic(u'who')
+get_ipython().magic(u'reset')
+get_ipython().magic(u'who')
+_i2
+_4
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {i : np.random.randn() for i in range(7)}
+data
+an_apple = 15
+an_apple = 30
+b = [1, 2, 3]
+b.append(3)
+b.count(3)
+b.__doc__
+path = '/home/khanhlq/PycharmProjects/python_data_build/chapter3/'
+b = pd.DataFrame({
+        "name": ['john', 'mary', 'anna'],
+        "age": [25, 23, 19]
+    })
+get_ipython().magic(u'pinfo b')
+a = 1
+get_ipython().magic(u'pinfo a')
+def add_numbers(a, b):
+    """
+    Add two numbers
+    
+    Returns: the sum of two numbers
+    """
+    return a + b
+get_ipython().magic(u'pinfo add_numbers')
+get_ipython().magic(u'pinfo2 pd.pivot_table')
+get_ipython().magic(u'run script.py')
+result
+get_ipython().magic(u"run sys_lab.py 12 89 'abc'")
+pront
+_i2
+_i3
+get_ipython().magic(u'logstart')
+ip_info = get_ipython().getoutput(u'ifconfig | grep "inet "')
+ip_info
